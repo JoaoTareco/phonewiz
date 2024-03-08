5546f932256961aaa44f836f52fac8f8ba7f5733
Joao Tareco <joao.tareco@eviso.it>
2024-03-08 18:57:27 +0000

    redirect after logout

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,15 +1,28 @@
-import { LandingNavbar } from "@/components/landing-navbar";
-import { LandingHero } from "@/components/landing-hero";
-import { LandingContent } from "@/components/landing-content";
+// import { LandingNavbar } from "@/components/landing-navbar";
+// import { LandingHero } from "@/components/landing-hero";
+// import { LandingContent } from "@/components/landing-content";
+
+// const LandingPage = () => {
+//   return ( 
+//     <div className="h-full ">
+//       <LandingNavbar />
+//       <LandingHero />
+//       <LandingContent />
+//     </div>
+//    );
+// }
+ 
+// export default LandingPage;
+
+"use client";
+import { useEffect } from 'react';
 
 const LandingPage = () => {
-  return ( 
-    <div className="h-full ">
-      <LandingNavbar />
-      <LandingHero />
-      <LandingContent />
-    </div>
-   );
+ useEffect(() => {
+    window.location.href = 'https://www.ctrlcap.com';
+ }, []);
+
+ return null; // Return null since we're redirecting and not rendering any content
 }
- 
+
 export default LandingPage;
