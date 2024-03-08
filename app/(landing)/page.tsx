4043ba7bca04c4876788db40ec650161bcd0a1a7
Joao Tareco <joao.tareco@eviso.it>
2024-03-08 19:05:31 +0000

    redirect after logout fix

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,28 +1,26 @@
-// import { LandingNavbar } from "@/components/landing-navbar";
-// import { LandingHero } from "@/components/landing-hero";
-// import { LandingContent } from "@/components/landing-content";
-
-// const LandingPage = () => {
-//   return ( 
-//     <div className="h-full ">
-//       <LandingNavbar />
-//       <LandingHero />
-//       <LandingContent />
-//     </div>
-//    );
-// }
- 
-// export default LandingPage;
-
-"use client";
-import { useEffect } from 'react';
+import { LandingNavbar } from "@/components/landing-navbar";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingContent } from "@/components/landing-content";
 
 const LandingPage = () => {
- useEffect(() => {
-    window.location.href = 'https://www.app.ctrlcap.com/sign-up';
- }, []);
-
- return null; // Return null since we're redirecting and not rendering any content
+  return ( 
+    <div className="h-full ">
+  
+    </div>
+   );
 }
-
+ 
 export default LandingPage;
+
+// "use client";
+// import { useEffect } from 'react';
+
+// const LandingPage = () => {
+//  useEffect(() => {
+//     window.location.href = 'https://www.app.ctrlcap.com/sign-up';
+//  }, []);
+
+//  return null; // Return null since we're redirecting and not rendering any content
+// }
+
+// export default LandingPage;
