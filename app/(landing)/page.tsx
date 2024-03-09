2e53268dca486c733993d2e89b0cba57781c0f3e
Joao Tareco <joao.tareco@eviso.it>
2024-03-09 17:37:41 +0000

    test redirect landing page internal

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,25 +2,25 @@
 // import { LandingHero } from "@/components/landing-hero";
 // import { LandingContent } from "@/components/landing-content";
 
-const LandingPage = () => {
-  return ( 
-    <div className="h-full ">
-  
-    </div>
-   );
-}
- 
-export default LandingPage;
-
-// "use client";
-// import { useEffect } from 'react';
-
 // const LandingPage = () => {
-//  useEffect(() => {
-//     window.location.href = 'https://www.app.ctrlcap.com/content-calendar';
-//  }, []);
-
-//  return null; // Return null since we're redirecting and not rendering any content
+//   return ( 
+//     <div className="h-full ">
+  
+//     </div>
+//    );
 // }
-
+ 
 // export default LandingPage;
+
+"use client";
+import { useEffect } from 'react';
+
+const LandingPage = () => {
+ useEffect(() => {
+    window.location.href = '/content-calendar';
+ }, []);
+
+ return null; // Return null since we're redirecting and not rendering any content
+}
+
+export default LandingPage;
