da23db5fbcecc4ecca2c6abb45d5aaefcd18b357
Joao Tareco <joao.tareco24@gmail.com>
2024-07-10 22:43:01 +0100

    heading

--- components/heading.tsx
+++ components/heading.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 interface HeadingProps {
   title: string;
   description: string;
-  icon: Icon;
+  icon?: Icon;
   iconColor?: string;
   bgColor?: string;
 }
@@ -20,11 +20,11 @@ export const Heading = ({
   return (
     <>
       <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-4">
-        <div className={cn("p-2 w-fit rounded-md", bgColor)}>
-          <Icon className={cn("w-10 h-10", iconColor)} />
-        </div>
+      {Icon &&  (<div className={cn("p-2 w-fit rounded-md", bgColor)}>
+         <Icon className={cn("w-8 h-8", iconColor)} />
+        </div>)}
         <div>
-          <h2 className="text-3xl font-bold">{title}</h2>
+          <h2 className="text-xl font-bold">{title}</h2>
           <p className="text-sm text-muted-foreground">
             {description}
           </p>
